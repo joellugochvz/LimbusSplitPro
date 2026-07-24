63dd0e87c366d6886955d40092f1ea6eb89cf7da
joellugochvz <joel.eduardo.lugo.chavez@gmail.com>
2026-07-23 18:47:28 -0600

    fix: Correct using directive System.Collections.ObjectModel in MainViewModel.cs

--- src/LimbusSplitPro.App/ViewModels/MainViewModel.cs
+++ src/LimbusSplitPro.App/ViewModels/MainViewModel.cs
@@ -1,4 +1,4 @@
-using System.Collections.ObservableCollection;
+using System.Collections.ObjectModel;
 using System.IO;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
